05c45f98c9a45f0d2fd3091d21d933ff2fe30c87
Rilly_ <chile.simon@4e4email.net>
2022-05-31 20:26:32 +0100

    implemneted list and retrieve endpoints and views

--- real_estate/listings/views.py
+++ real_estate/listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Listing
 
 # Create your views here.
@@ -11,3 +11,11 @@ def listing_list(request, *args, **kwargs):
         "listings": listings
     }
     return render(request, "listings.html", context)
+
+
+def listing_retrieve(request, id, *args, **kwargs):
+    listing = Listing.objects.get(id=id)
+    context = {
+        "listing": listing
+    }
+    return render(request, "retrieve_listing.html", context)
